08f65ab4e69ab7ae64753ffbed81b9d0965dfdd2
rvdegroen <rvdegroen@gmail.com>
2023-04-17 22:16:33 +0200

    integrated ejs

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// IMPORTS
 const express = require("express");
-const app = express();
 const http = require("http");
+
+// VARIABLES
+const app = express();
 const server = http.createServer(app);
+
+// MIDDLEWARE
 app.set("port", process.env.PORT || 3000);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-	res.send("Hello World");
+	res.render("index");
 });
 
 app.listen(3000);
